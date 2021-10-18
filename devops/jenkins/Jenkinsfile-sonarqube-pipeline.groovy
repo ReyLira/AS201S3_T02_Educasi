59fb0d0366c113f1d48e8f42a02c3bc18d44f358
JCardenas2018 <jesus.cardenas@vallegrande.edu.pe>
2021-10-18 10:30:09 -0500

    Se agrega pipeline sonarqube en jenkins

---
+++ devops/jenkins/Jenkinsfile-sonarqube-pipeline.groovy
@@ -0,0 +1,43 @@
+pipeline {
+    agent none
+    stages {
+
+        stage("Git Clone"){
+            steps {
+                cleanWs()
+                    checkout([$class: 'GitSCM', 
+                    branches: [[name: '*/feature/sonarqube']], 
+                    doGenerateSubmoduleConfigurations: false, 
+                    extensions: [[$class: 'CleanCheckout']], 
+                    submoduleCfg: [], 
+                    userRemoteConfigs: [
+                        [url: 'https://github.com/vallegrande/AS201S3_T02_Educasi.git', credentialsId: 'jenkins_github']
+                        ]])
+                sh 'pwd' 
+                sh 'ls -l'
+            } //steps
+        }  //stage
+
+        stage('Build Project') {
+            agent any
+            steps {
+                    sh 'mvn clean install'
+            }
+        }
+
+        stage('SonarQube Analysis') {
+            agent any
+            steps {
+                    sh 'sonar-scanner'
+            }
+        }
+
+        stage('Quality Gate') {
+            steps {
+                timeout(time: 1, unit: 'HOURS') {
+                    waitForQualityGate abortPipeline: true
+                }
+            }
+        }
+    }
+}
