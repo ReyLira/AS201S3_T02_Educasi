ce2546a7d59fef31e334b7621405e25da155a4f0
JCardenas2018 <43141971+JCardenas2018@users.noreply.github.com>
2021-10-19 17:07:37 -0500

    Update Jenkinsfile-sonarqube-pipeline.groovy

--- devops/jenkins/Jenkinsfile-sonarqube-pipeline.groovy
+++ devops/jenkins/Jenkinsfile-sonarqube-pipeline.groovy
@@ -28,7 +28,7 @@ pipeline {
         stage('SonarQube Analysis') {
             agent any
             steps {
-                    sh 'mvn clean verify sonar:sonar -Dsonar.host.url=http://35.222.30.201:9400 -Dsonar.login=e49716356b537e63b2d17480fbf070f84dd623b9'
+                    sh 'mvn clean verify sonar:sonar -Dsonar.host.url=http://35.222.30.201:9400'
             }
         }
 
